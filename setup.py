288c2343df528a12a96fba319f88f3787d2141db
jaclew <42406523+jaclew@users.noreply.github.com>
2022-12-19 10:23:41 +0100

    Add files via upload
    restructure code upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='repgenr',
+    version='1.0',
+    author='jaclew',
+    description='no_description',
+    packages=['repgenr'],
+    scripts=['repgenr/repgenr','repgenr/metadata.py','repgenr/genome.py','repgenr/derep.py','repgenr/phylo.py','repgenr/tree2tax.py']
+)
